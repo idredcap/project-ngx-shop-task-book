d69072da7f6db551b96ccdd6514b61b05959a767
#RedCap <idredcap@gmail.com>
2020-09-13 17:12:51 +0300

    feat: [Product card] Added @Input & @Output

--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,11 @@
 import {
   Component,
   ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
 } from '@angular/core';
+import { IProduct } from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -10,5 +14,15 @@ import {
 })
 export class CategoryProductComponent {
 
+  @Input()  public product: IProduct = {} as IProduct;
+  @Output() public addToCart: EventEmitter<string> = new EventEmitter<string>();
+  @Output() public goToProduct: EventEmitter<string> = new EventEmitter<string>();
 
+  public addToBasket(): void {
+    this.addToCart.emit('Вы добавили товар в корзину');
+  }
+
+  public redirectTo(): void {
+    this.goToProduct.emit('Переход на компонент товара');
+  }
 }
