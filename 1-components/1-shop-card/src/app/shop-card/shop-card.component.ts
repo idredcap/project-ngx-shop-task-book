28e1ffee83c5b6129407de495cf89d645f98651d
#RedCap <idredcap@gmail.com>
2020-09-13 16:34:36 +0300

    feat: [Shop card] @Input product

--- 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
+++ 1-components/1-shop-card/src/app/shop-card/shop-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input, EventEmitter, Output} from '@angular/core';
+import {ICartProduct} from '../../../../../shared/mocks/1-components/cart-product';
 
 @Component({
   selector: 'app-shop-card',
   templateUrl: './shop-card.component.html',
+  styleUrls:   ['./shop-card.component.scss']
 })
-
 export class ShopCardComponent {
+  @Input()  public product: ICartProduct = {} as ICartProduct;
+  @Output() public increment = new EventEmitter();
+  @Output() public decrement = new EventEmitter();
 
+  public decrementProductInCart(): void {
+    this.decrement.emit();
+  }
+
+  public incrementProductInCart(): void {
+    this.increment.emit();
+  }
 }
